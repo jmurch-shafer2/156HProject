e6b56f75423c079f5c594f4912dffd7f888aba60
Joel <jmurchshafer@gmail.com>
2020-02-11 20:44:13 -0600

    added some things to make it work

--- src/cse/unl/Testing.java
+++ src/cse/unl/Testing.java
@@ -2,17 +2,26 @@ package cse.unl;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.net.URL;
 import java.util.Scanner;
+/**
+ * 
+ * @author Natalie Ruckman and Joel Murch-Shafer
+ *
+ */
 
 public class Testing {
 	public static void main(String[] argc) {
-		URL url = getClass().getResource("Persons.dat");
-		File path = new File(url.toURI());
-		System.out.println(path.exists());
+//		URL url = getClass().getResource("Persons.dat");
+		
+		File path = new File("C://Users//jmurc//Documents//GitHub//EclipseRootDir//MurckmanConsulting//Assets.dat");
+		System.out.println(path.getAbsolutePath());
 		Scanner scan = null;
 		try {
 			scan = new Scanner(path);
+			while(scan.hasNextLine()) {
+				System.out.println(scan.nextLine());
+				//TODO Call constructor too
+			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -20,9 +29,7 @@ public class Testing {
 		
 		int numOfLines = scan.nextInt();
 		
-		for(int i = 0; i<numOfLines;i++) {
-			System.out.println(scan.hasNextLine());
-		}
+
 		
 		
 		scan.close();
