e72938367fc78bea02bb8ece4a558bf93cf9a2cd
natalieruckman359 <ruckman.natalie@gmail.com>
2020-02-11 18:53:35 -0600

    added Assets.java starting code

--- src/cse/unl/Assets.java
+++ src/cse/unl/Assets.java
@@ -1,6 +1,32 @@
+//separate classes for each type of asset?
 package cse.unl;
 
-public abstract class Assets {
-	String balance;
-	
+//should we use implement to have an owned to connect to person class
+public abstract class Asset implements Owned{
+  protected String code; //account code or owner code?
+  protected String assetType;
+  protected String label;
+
+
+}
+
+//need to check assetType from file and construct appropriate child class
+class DepositAccount extends Asset {
+  private double apr;
+}
+
+class PrivateInvestment extends Asset {
+  //should these be doubles?
+  private double quarterlyDividend;
+  private double baseRateOfReturn;
+  private double baseOmegaMeasure;
+  private double totalValue;
+}
+
+class Stock extends Asset {
+  private double quarterlyDividend;
+  private double baseRateOfReturn;
+  private double betaMeasure;
+  private double stockSymbol;
+  private double sharePrice;
 }
