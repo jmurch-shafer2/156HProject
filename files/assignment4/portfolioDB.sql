754b08bffb73c2cf3cfde624e3bfe6fbf3f4f30f
nruckman359 <55301643+nruckman359@users.noreply.github.com>
2020-03-10 19:34:18 -0500

    Added Asset and AssetPortfolio

--- files/assignment4/portfolioDB.sql
+++ files/assignment4/portfolioDB.sql
@@ -7,17 +7,19 @@ drop table if exists Portfolio;
 drop table if exists Address;
 drop table if exists Email;
 drop table if exists Person;
+drop table if exists PortfolioAsset;
+drop table if exists Asset;
 
 create table Person(
 	personId int not null primary key auto_increment,
-    firstName varchar(50) not null,
-    lastName varchar(50) not null,
-    brokerType varchar(1),
-    secIdentifier varchar(10)    
+    firstName varchar(255) not null,
+    lastName varchar(255) not null,
+    brokerType varchar(255),
+    secIdentifier varchar(255)    
 );
 create table Email(
 	emailId int not null primary key auto_increment,
-	email varchar(100) not null,
+	email varchar(255) not null,
     personId int not null,
     foreign key (personId) references Person(personId)
 );
@@ -25,11 +27,24 @@ create table Email(
 create table Address(
 	addressId int not null primary key auto_increment,
     personId int not null,
-    street varchar(50) not null,
-    city varchar(50) not null,
+    street varchar(255) not null,
+    city varchar(255) not null,
     zipCode int not null,
-    country varchar(50),
+    country varchar(255),
 	foreign key (personId) references Person(personId)
+);
+
+create table Asset(
+	assetId int not null primary key auto_increment, 
+	typeOfAsset varchar(255) not null
+);
+
+create table PortfolioAsset(
+	portfolioAssetId int not null primary key auto_increment,
+	assetId int not null,
+	portfolioId int not null,
+	foreign key (assetId) references Asset(assetId),
+	foreign key (portfolioId) references Portfolio(portfolioId)
 );
 
 create table Portfolio(
@@ -37,41 +52,40 @@ create table Portfolio(
     ownerId int not null,
     managerId int not null,
     beneficiaryId int,
+    foreign key (beneficiaryId) references Person(personId),
     foreign key (ownerId) references Person(personId),
     foreign key (managerId) references Person(personId)
 );
 
-
-
 create table DepositAccount(
 	depositAccountId int not null primary key auto_increment,
-	portfolioId int not null,
+	assetId int not null,
     apr int not null,
     totalValue double not null,
-    foreign key (portfolioId) references Portfolio(portfolioId)
+    foreign key (assetId) references Asset(assetId)
 );
 
 create table PrivateInvestment(
 	privateInvestmentId int not null primary key auto_increment,
-	portfolioId int not null,
+	assetId int not null,
     baseRateReturn double not null,
     quarterlyDividend double not null,
     baseOmegaMeasure double not null,
     totalValue double not null,
     percentageOwned double not null,
-    foreign key (portfolioId) references Portfolio(portfolioId)
+    foreign key (assetId) references Asset(assetId)
 );
 
 create table Stock(
 	stockId int not null primary key auto_increment,
-	portfolioId int not null,
-    stockSymbol varchar(10) not null,
+	assetId int not null,
+    stockSymbol varchar(255) not null,
     sharesOwned int not null,
     quarterlyDividend double not null,
     sharePrice double not null,
     betaMeasure double not null,
     baseRateReturn double not null,
-    foreign key (portfolioId) references Portfolio(portfolioId)
+    foreign key (assetId) references Asset(assetId)
 );
 
 insert Person(firstName, lastName, brokerType, secIdentifier) values ("Brent","Elphinston","","");
