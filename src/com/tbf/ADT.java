c90bed1489b8d3c6010ca718290e4d6320d0732e
Joel <jmurchshafer@gmail.com>
2020-04-20 15:30:31 -0500

    update

--- src/com/tbf/ADT.java
+++ src/com/tbf/ADT.java
@@ -1,5 +1,27 @@
 package com.tbf;
 
+import unl.cse.trucks.Truck;
+import unl.cse.trucks.TruckListNode;
+
 public class ADT {
-	
+
+    private TruckListNode next;
+    private Truck item;
+
+    public TruckListNode(Truck item) {
+        this.item = item;
+        this.next = null;
+    }
+
+    public Truck getTruck() {
+        return item;
+    }
+
+    public TruckListNode getNext() {
+        return next;
+    }
+
+    public void setNext(TruckListNode next) {
+        this.next = next;
+    }
 }
